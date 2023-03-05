d22e369312ea27d4b298f444210d4db0e170da66
frejramstrom <frejramstrom1@gmail.com>
2023-03-05 17:27:44 +0100

    Balloon done and working on ground

--- maingame.js
+++ maingame.js
@@ -1,15 +1,42 @@
 const x = 100;
 const y = 100;
 
-//Red balloon//
+//background//
 background(46, 68, 130);
+
+//Fire Balloon//
+fill(252, 100, 0);
+noStroke(0);
+triangle(x - 50, y, x, y + 75, x + 50, y);
+
+fill(215, 53, 2);
+noStroke(0);
+triangle(x - 40, y, x, y + 75, x + 40, y);
+
+fill(199, 0, 57);
+noStroke(0);
+triangle(x - 30, y, x, y + 75, x + 30, y);
+
+fill(255, 255, 0);
+noStroke(0);
+triangle(x - 10, y, x, y + 75, x + 10, y);
+
+fill(0, 255, 255);
+noStroke(0);
+triangle(x - 1, y, x, y + 75, x + 1, y);
+
+//Red balloon//
+
 fill(199, 0, 57);
 noStroke();
 ellipse(x, y, 100, 110);
 
-//Lines connecting to Balloon from left side//
+//Lines connecting to Balloon from left side + ropes between with curves//
 strokeWeight(2);
 stroke(255, 195, 0);
+curve(x - 80, y - 100, x - 40, y, x + 40, y, x + 80, y - 100);
+curve(x - 70, y - 100, x - 30, y, x + 30, y, x + 70, y - 100);
+curve(x - 60, y - 100, x - 20, y, x + 20, y, x + 60, y - 100);
 line(x - 40, y, 70, 180);
 line(x - 30, y, 80, 180);
 line(x - 20, y, 90, 180);
@@ -54,3 +81,6 @@ noStroke();
 ellipse(x - 20, y + 100, 15, 20);
 ellipse(x, y + 100, 15, 20);
 ellipse(x + 20, y + 100, 15, 20);
+
+//ground//
+rect(x - 100, y + 350, 800, 40);
